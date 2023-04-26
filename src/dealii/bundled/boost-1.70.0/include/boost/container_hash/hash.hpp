e28655b875cfa53cc795b6924807ed19e44294bc
John Henning <john.henning@oracle.com>
2023-04-26 17:51:23 -0400

    Avoid use of std::unary_function From: Wolfgang Bangerth <bangerth@colostate.edu> via: https://github.com/bangerth/spec-cpuv8-sampleflow/commit/56ea0c0353f150d873f5671096bc048afc15d641.patch

--- src/dealii/bundled/boost-1.70.0/include/boost/container_hash/hash.hpp
+++ src/dealii/bundled/boost-1.70.0/include/boost/container_hash/hash.hpp
@@ -118,17 +118,12 @@ namespace boost
 {
     namespace hash_detail
     {
-#if defined(_HAS_AUTO_PTR_ETC) && !_HAS_AUTO_PTR_ETC
         template <typename T>
         struct hash_base
         {
             typedef T argument_type;
             typedef std::size_t result_type;
         };
-#else
-        template <typename T>
-        struct hash_base : std::unary_function<T, std::size_t> {};
-#endif
 
         struct enable_hash_value { typedef std::size_t type; };
 
